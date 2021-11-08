d58074245de5d829a066c7c00b39e6b6928beb4b
Manu <manueluismartin@gmail.com>
2021-11-08 12:40:21 +0000

    Migrations not working - issue w root/pass

---
+++ migrations/081120211221-CreateEmailTable.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Email", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Email");
+  },
+};
